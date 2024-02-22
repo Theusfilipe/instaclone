27e3e87211d3447e17ebc0e44ac10f77fe52c3c0
Theusfilipe <theusfilipe@gmail.com>
2024-02-22 13:57:57 -0300

    Sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users', to: 'devise/registration#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end 
   devise_for :users
   get 'home/about'
   resources :posts
@@ -7,6 +12,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  #devise
+  
 
   # Defines the root path route ("/")
   root "posts#index"
