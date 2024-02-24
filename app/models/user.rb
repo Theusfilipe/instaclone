61ca8983a677a6a04c1ea370537e4cd8a68dedfb
Theusfilipe <theusfilipe@gmail.com>
2024-02-24 17:01:53 -0300

    added followability to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+        followability
+
         has_many :posts
 
         has_many :likes
@@ -13,6 +15,11 @@ class User < ApplicationRecord
         has_one_attached :avatar
 
         before_create :randomize_id
+
+        def unfollow(user)
+          followerable_relationships.where(followable_id: user.id).destroy_all
+        end
+
         private
         def randomize_id
           begin
